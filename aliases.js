4b3bea43511431e5c91d25b0c3d03b7f7dbec400
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-03 09:19:38 +0000

    Deploy rust-analyzer/rust-analyzer to github.com/rust-analyzer/rust-analyzer.git:gh-pages

--- aliases.js
+++ aliases.js
@@ -12,6 +12,7 @@ ALIASES["ra_ide_api"] = {};
 ALIASES["ra_lsp_server"] = {};
 ALIASES["ra_mbe"] = {};
 ALIASES["ra_parser"] = {};
+ALIASES["ra_prof"] = {};
 ALIASES["ra_project_model"] = {};
 ALIASES["ra_syntax"] = {};
 ALIASES["ra_text_edit"] = {};
---
+++ ra_prof/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["profile",""],["set_filter",""]],"struct":[["Filter",""],["Profiler",""]]});
